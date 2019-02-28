178c8d383dc1d1210c73ddc36878aed130a61314
Jonathan <jsaavedra@optimisa.cl>
2019-02-28 10:58:38 -0300

    Generacion del controlador y vistas usando scaffolding a partir del model, seccion 4, clase 12

--- Sistema/Startup.cs
+++ Sistema/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Models;
 
 namespace Sistema
 {
@@ -33,6 +35,9 @@ namespace Sistema
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SistemaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SistemaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
