345168e9c4d3cdfdcc22296ce524216250ce757a
Jonathan <jsaavedra@optimisa.cl>
2019-02-28 10:10:25 -0300

    Creacion model categoria, cambios para seccion 4, clase 11

---
+++ Sistema/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+	public class Categoria
+	{
+		public int CategoriaID { get; set; }
+		[Required (ErrorMessage ="Campo Requerido")]
+		[StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe terner de 3 a 50 caracteres")]
+		public String Nombre { get; set; }
+		[StringLength(256, ErrorMessage ="La descripción no debe exceder los 256 caracteres")]
+		[Display(Name ="Descripción")]
+		public String Descripcion { get; set; }
+		public Boolean Estado { get; set; }
+	}
+}
