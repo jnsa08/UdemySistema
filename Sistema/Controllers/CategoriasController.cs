595e1c2f36490d171d55483abbbdd844ea5e112c
Jonathan <jsaavedra@optimisa.cl>
2019-02-28 15:21:28 -0300

    Ordenar grilla a partir de la columna seleccionada, session 4, clase 16

--- Sistema/Controllers/CategoriasController.cs
+++ Sistema/Controllers/CategoriasController.cs
@@ -19,9 +19,31 @@ namespace Sistema.Controllers
         }
 
         // GET: Categorias
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
-            return View(await _context.Categoria.ToListAsync());
+			ViewData["NombreSortParam"] = String.IsNullOrEmpty(sortOrder) ? "nombre_desc" : "";
+			ViewData["DescripcionSortParam"] = (sortOrder == "descripcion_asc") ? "descripcion_desc" : "descripcion_asc";
+
+			var categorias = from s in _context.Categoria select s;
+
+			switch (sortOrder)
+			{
+				case "nombre_desc":
+					categorias = categorias.OrderByDescending(s => s.Nombre);
+					break;
+				case "descripcion_desc":
+					categorias = categorias.OrderByDescending(s => s.Descripcion);
+					break;
+				case "descripcion_asc":
+					categorias = categorias.OrderBy(s => s.Descripcion);
+					break;
+				default:
+					categorias = categorias.OrderBy(s => s.Nombre);
+					break;
+
+			}
+			return View(await categorias.AsNoTracking().ToListAsync());
+			//return View(await _context.Categoria.ToListAsync());
         }
 
         // GET: Categorias/Details/5
